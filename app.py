d3f685461594703e82b4fdd3c813be78295a9a3f
Christine Wu <z5209380@unsw.edu.au>
2019-07-13 15:24:27 +1000

    useless commit

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
+import uuid 
+import boto3
+s3_client = boto3.client('s3')
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
+    # if request.method == "POST":
+    #     if 'translate' in request.form:
+    #         # initiate polly 
+    #         # mp3 file is formed
+    #         # playback mp3 file 
+    #         translate_speech = 1
+    #         return render_template('home.html', translate_speech=translate_speech)
+
     return render_template('home.html')
